893dbc6fd924362af847fc63e80b528de648450a
Junwoo Hwang <no.water.in.the.house@gmail.com>
2020-06-16 15:47:24 +0900

    200616 Django Form Class

--- Django/formclass/articles/urls.py
+++ Django/formclass/articles/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('new/', views.new, name='new'),
     path('<int:pk>/', views.detail, name='detail'),
+    path('<int:pk>/delete/', views.delete, name='delete'),
+    path('<int:pk>/edit/', views.edit, name='edit'),
 ]
--- Django/formclass/articles/views.py
+++ Django/formclass/articles/views.py
@@ -20,12 +20,14 @@ def new(request):
         
         # 2-1. data 유효성 검사
         if form.is_valid():
-            # 2-2. 검증된 data 꺼내오기
-            title = form.cleaned_data.get('title')
-            content = form.cleaned_data.get('content')
-            # 2-3. Database에 저장
-            article = Article(title=title, content=content)
-            article.save()
+            # (ModelForm) 2-2. Database에 저장
+            article = form.save()
+            # # 2-2. 검증된 data 꺼내오기
+            # title = form.cleaned_data.get('title')
+            # content = form.cleaned_data.get('content')
+            # # 2-3. Database에 저장
+            # article = Article(title=title, content=content)
+            # article.save()
             # 3. 저장된 data를 확인할 수 있는 곳으로 안내
             return redirect('articles:detail', article.pk)
 
@@ -46,3 +48,42 @@ def detail(request, pk):
         'article': article,
     }
     return render(request, 'articles/detail.html', context)
+
+
+def delete(request, pk): # POST
+    article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+        article.delete()
+    return redirect('articles:index')
+
+def edit(request, pk):
+    # 1. Database에서 data 가져오기
+    article = Article.objects.get(pk=pk)
+
+    if request.method == 'POST':
+        # data 수정!
+        
+        # (ModelForm) 2-1. form에 data 집어넣기 + instance와 연결
+        form = ArticleForm(request.POST, instance=article)
+        # # 2-1. form에 data 집어넣기(검증 목적)
+        # form = ArticleForm(request.POST)
+        # 2-2. form에서 data 유효성 검사
+        if form.is_valid():
+            # (ModelForm) 2-3. Database에 저장
+            article = form.save()
+            # # 2-3. 검증된 data를 반영하기(저장)
+            # article.title = form.cleaned_data.get('title')
+            # article.content = form.cleaned_data.get('content')
+            # article.save()
+            # 3. 저장된 내용을 확인할 수 있는 페이지로 안내
+            return redirect('articles:detail', article.pk)
+    else:  
+        # 수정 양식 보여주기!
+        # (ModelForm) 2. Form에 data 채워 넣기
+        form = ArticleForm(instance=article)
+        # # 2. Form에 data 채워 넣기
+        # form = ArticleForm(initial=article.__dict__)
+    context = {
+        'form': form,
+    }
+    return render(request, 'articles/edit.html', context)
