1637e8dfd810bc8a5ff37974ebd9586715ff5ee8
Junwoo Hwang <no.water.in.the.house@gmail.com>
2020-06-17 14:32:09 +0900

    200617 Django 1:N Relation

--- Django/formclass/articles/urls.py
+++ Django/formclass/articles/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('<int:pk>/', views.detail, name='detail'),
     path('<int:pk>/delete/', views.delete, name='delete'),
     path('<int:pk>/edit/', views.edit, name='edit'),
+    # /articles/1/comments/new/
+    path('<int:article_pk>/comments/new/', views.comments_new, name='comments_new'),
+    path('<int:article_pk>/comments/<int:pk>/delete/', views.comments_delete, name='comments_delete'),
+    path('<int:article_pk>/comments/<int:pk>/edit/', views.comments_edit, name='comments_edit'),
 ]
