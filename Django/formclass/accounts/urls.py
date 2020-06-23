80101b57c736ac61c0ac18c6d26d8564b300e6bb
Junwoo Hwang <no.water.in.the.house@gmail.com>
2020-06-23 14:56:00 +0900

    200623 Django Auth (PM)

--- Django/formclass/accounts/urls.py
+++ Django/formclass/accounts/urls.py
@@ -4,7 +4,10 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
+    path('signup/', views.signup, name='signup'), # User [C]reate
+    path('login/', views.login, name='login'), # Session [C]reate
+    path('logout/', views.logout, name='logout'), # Session [D]elete
+    path('delete/', views.delete, name='delete'), # User [D]elete
+    path('edit/', views.edit, name='edit'), # User [U]pdate
+    path('password/', views.password, name='password'), # (Password) [U]pdate
 ]
--- Django/formclass/accounts/views.py
+++ Django/formclass/accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
 from django.contrib.auth import login as auth_login, logout as auth_logout
+from .forms import CustomUserChangeForm
+from django.contrib.auth import update_session_auth_hash
 
 
 def signup(request):
@@ -61,3 +63,59 @@ def logout(request): # POST
         # Logout!
         auth_logout(request)
     return redirect('articles:index')
+
+
+def delete(request): # POST
+    # 만약 로그인이 안되어 있다면, index로 보내기
+    if not request.user.is_authenticated:
+        return redirect('articles:index')
+
+    # User 삭제
+    if request.method == 'POST':
+        request.user.delete()
+
+    return redirect('articles:index')
+
+
+def edit(request):
+    user = request.user
+    if request.method == 'POST':
+        # User 업데이트!
+        # 1. POST로 넘어온 데이터 form에 넣기
+        form = CustomUserChangeForm(request.POST, instance=user)
+        # 2. form에서 데이터 검증하기
+        if form.is_valid():
+            # 3. 검증 통과하면, database에 저장
+            form.save()
+            # 4. 업데이트 결과 확인이 가능한 페이지로 안내
+            return redirect('articles:index')
+    else:
+        # User 업데이트 양식 보여주기
+        form = CustomUserChangeForm(instance=user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/edit.html', context)
+
+
+def password(request):
+    user = request.user
+    if request.method == 'POST':
+        # Password 변경!
+        # 1. POST로 넘어온 data를 form에 넣기
+        form = PasswordChangeForm(user, request.POST)
+        # 2. form 유효성 검사
+        if form.is_valid():
+            # 3. 검사를 통과했다면, 저장!
+            user = form.save()
+            # 3-1. 저장 완료 후, 로그인 세션 유지!
+            update_session_auth_hash(request, user)
+            # 4. 어딘가로 돌려보내기
+            return redirect('accounts:edit')
+    else:
+        # Password 변경 양식 보여주기
+        form = PasswordChangeForm(user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/password.html', context)
